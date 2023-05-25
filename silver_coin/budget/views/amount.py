e82f46c45c4f77dbbef0ecc80d88cfa875195cb8
Connor <connora.wellington18@gmail.com>
2023-05-26 06:20:35 +1200

    Add Login Mixin to views.

--- silver_coin/budget/views/amount.py
+++ silver_coin/budget/views/amount.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from django.urls import reverse
 
 from ..models import Budget
 from ..models import Amount
 
-class AmountList(ListView):
+class AmountList(LoginRequiredMixin, ListView):
     model = Amount
     template_name = "amount/amount_list.html"
     context_object_name = "amounts"
     
+    def get_login_url(self):
+        return reverse("login")
 
     def get_queryset(self):
         """
