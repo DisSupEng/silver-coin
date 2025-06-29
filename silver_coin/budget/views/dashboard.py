a72f5815a4d24f36e211b0ee93c291859ab2c2eb
Connor <DisSupEng>
2025-06-29 13:57:32 +1200

    Fix dashboard recent net amount with no budget period.

--- silver_coin/budget/views/dashboard.py
+++ silver_coin/budget/views/dashboard.py
@@ -12,7 +12,9 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         Override to check if the user has a budget defined.
         """
         has_budget = Budget.objects.filter(owner=request.user).count() > 0
-        recent_net_amount = BudgetPeriod.objects.filter(budget__owner=request.user).last().net_amount()
+        last_period = BudgetPeriod.objects.filter(budget__owner=request.user).last()
+
+        recent_net_amount = last_period.net_amount() if last_period else None
 
         return super().get(request, has_budget=has_budget, recent_net_amount=recent_net_amount)
 
